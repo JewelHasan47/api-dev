1e652a832601a4b0ceff53da4ffd0c8010eeefa7
Jewel Hasan <jewelhasan477@gmail.com>
2024-03-25 16:03:50 +0600

    latest

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
-use GuzzleHttp\Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\LogoutRequest;
 use App\Http\Requests\Auth\RegistrationRequest;
@@ -15,7 +15,7 @@ class AuthController extends Controller {
     public function registration( RegistrationRequest $request ) {
         $name     = $request->name;
         $email    = $request->email;
-        $password = bcrypt( $request->password );
+        $password = $request->password;
 
         $user = User::create( [
             'name'     => $name,
@@ -23,7 +23,7 @@ class AuthController extends Controller {
             'password' => Hash::make( $password ),
         ] );
 
-        $accessToken = $user->createToken( 'Personal Access Token' )->accessToken;
+        $accessToken = $user->createToken( 'accessToken' )->accessToken;
 
         return response( [
             'id'           => $user->id,
@@ -34,28 +34,25 @@ class AuthController extends Controller {
     }
 
     public function login( LoginRequest $request ) {
-        $http = new Client();
 
         $user = User::where( 'email', $request->email )->first();
 
-        if ( !$user ) {
+        if ( !$user || !Hash::check( $request->password, $user->password ) ) {
             return response()->json( [
                 'message' => 'Email or Password incorrect.',
             ], 401 );
         }
 
-        $response = $http->post( route( 'http://127.0.0.1:8000/oauth/token' ), [
-            'form_params' => [
-                'grant_type'    => 'password',
-                'client_id'     => env( 'CLIENT_ID' ),
-                'client_secret' => env( 'CLIENT_SECRET' ),
-                'username'      => $user->email,
-                'password'      => $request->password,
-                'scope'         => '',
-            ],
+        $response = Http::asForm()->post( 'http://127.0.0.1:8000/oauth/token', [
+            'grant_type'    => 'password',
+            'client_id'     => env( 'CLIENT_ID_PASSPORT' ),
+            'client_secret' => env( 'CLIENT_SECRET_PASSPORT' ),
+            'username'      => $user->email,
+            'password'      => $user->password,
+            'scope'         => '*',
         ] );
 
-        return $response->getBody();
+        return $response->json();
     }
 
     public function logout( LogoutRequest $request ) {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,32 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\AuthCode;
+use Laravel\Passport\Client;
+use Laravel\Passport\PersonalAccessClient;
+use Laravel\Passport\RefreshToken;
+use Laravel\Passport\Token;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        Passport::enablePasswordGrant();
+
+        Passport::useTokenModel( Token::class );
+        Passport::useRefreshTokenModel( RefreshToken::class );
+        Passport::useAuthCodeModel( AuthCode::class );
+        Passport::useClientModel( Client::class );
+        Passport::usePersonalAccessClientModel( PersonalAccessClient::class );
     }
 }
